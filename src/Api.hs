2216d74cb5e1b7b0ee00b6df7fb6649737d3b820
Fernando Rincon <fernando.rincon@valantmed.com>
2017-07-04 23:03:47 +0200

    Add list packages endpoing

--- src/Api.hs
+++ src/Api.hs
@@ -7,12 +7,16 @@ module Api
 
 import Development.Placeholders
 import Data.Maybe (fromJust)
+import Data.Aeson (encode)
 import System.IO (withBinaryFile, IOMode(WriteMode))
+import System.Directory (listDirectory, getCurrentDirectory, doesFileExist)
+import System.FilePath (takeFileName)
 import Codec.Binary.UTF8.String (decode)
-import Control.Monad (unless)
+import Control.Monad (unless, filterM)
 import Network.Wai (Application, requestMethod, responseLBS, requestBody, responseFile)
 import Network.HTTP.Types.Method (methodGet, methodPost, methodPut)
-import Network.HTTP.Types.Status (ok200, noContent204)
+import Network.HTTP.Types.Header (hContentType)
+import Network.HTTP.Types.Status (ok200, noContent204, methodNotAllowed405)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.Char8 as LC
@@ -28,21 +32,35 @@ v1 = prependRoute "/packages" packages
 
 packages :: RoutesMap IO
 packages =
-  [ ("/:packageName", packagesHandler)
+  [ ("/", listPackagesHandler)
+  , ("/:packageName", packagesHandler)
   ]
 
+listPackagesHandler :: Handler IO
+listPackagesHandler p rq respond = case requestMethod rq of
+  method | method == methodGet -> listPackages p rq respond
+  _ -> respond $ responseLBS methodNotAllowed405 [] LC.empty
+
+listPackages :: Handler IO
+listPackages _ rq respond = do
+  currDir <- getCurrentDirectory
+  allEntries <- listDirectory currDir
+  onlyFiles <- filterM doesFileExist allEntries
+  let onlyFileNames = map takeFileName onlyFiles
+  respond $ responseLBS ok200 [(hContentType, "application/json")] (encode onlyFileNames)
+
 packagesHandler :: Handler IO
-packagesHandler p rq = case requestMethod rq of
-  method | method == methodGet -> getPackage p rq
-  method | method == methodPut -> putPackage p rq
-  _ -> $notImplemented
+packagesHandler p rq respond = case requestMethod rq of
+  method | method == methodGet -> getPackage p rq respond
+  method | method == methodPut -> putPackage p rq respond
+  _ -> respond $ responseLBS methodNotAllowed405 [] LC.empty
 
 packageName :: [(B.ByteString, B.ByteString)] -> String
 packageName params = decode $ B.unpack $ fromJust $ lookup "packageName" params
 
 getPackage :: Handler m
 getPackage params rq respond =
-  respond $ responseFile ok200 [] (packageName params) Nothing
+  respond $ responseFile ok200 [(hContentType, "application/octet-stream")] (packageName params) Nothing
 
 putPackage :: Handler IO
 putPackage params rq respond = do
